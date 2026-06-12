a44192b4a25563456d3201764ad5d7ac6f7b63dd
Aashish Shinde <aashishshinde.work@gmail.com>
2026-06-12 15:53:05 +0530

    Add files via upload

---
+++ Day11_Java/LongestWord.java
@@ -0,0 +1,29 @@
+package Day10_placement;
+
+import java.util.Scanner;
+
+public class LongestWord {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter a sentence: ");
+		String sentence = sc.nextLine();
+
+		String[] words = sentence.split(" ");
+
+		String longestWord = "";
+
+		for (String word : words) {
+
+			if (word.length() > longestWord.length()) {
+				longestWord = word;
+			}
+		}
+
+		System.out.println("Longest Word: " + longestWord);
+
+		sc.close();
+	}
+}
