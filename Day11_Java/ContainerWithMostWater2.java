57de3caca18c6ccfef1e7a3a2298c69d0a248bd5
Aashish Shinde <aashishshinde.work@gmail.com>
2026-06-12 15:16:23 +0530

    Add files via upload

---
+++ Day11_Java/ContainerWithMostWater2.java
@@ -0,0 +1,34 @@
+package Day10_placement;
+
+public class ContainerWithMostWater2 {
+
+	    public static void main(String[] args) {
+
+	        int[] height = {8, 8, 6, 2, 5, 8, 4, 3, 7};
+
+	        int left = 0;
+	        int right = height.length - 1;
+
+	        int maxArea = 0;
+
+	        while (left < right) {
+
+	            int width = right - left;
+
+	            int area = Math.min(height[left], height[right]) * width;
+
+	            if (area > maxArea) {
+	                maxArea = area;
+	            }
+
+	            if (height[left] < height[right]) {
+	                left++;
+	            } else {
+	                right--;
+	            }
+	        }
+
+	        System.out.println("Maximum Area = " + maxArea);
+	    }
+	}
+
