2af79f0e2f9916f1c084a467f67d9ad6067c331a
Aashish Shinde <aashishshinde.work@gmail.com>
2026-06-21 22:41:48 +0530

    Add files via upload

---
+++ Day16_Questions/LinkedListDemo.java
@@ -0,0 +1,28 @@
+// Question: Create a Linked List and display it
+
+class Node {
+
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
+
+public class LinkedListDemo {
+
+    public static void main(String[] args) {
+
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+
+        Node temp = head;
+
+        while(temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
