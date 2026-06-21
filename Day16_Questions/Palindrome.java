5e90916ebde96ef9f8a87aa10b063e42d66ddf22
Aashish Shinde <aashishshinde.work@gmail.com>
2026-06-21 22:25:50 +0530

    Add files via upload

---
+++ Day16_Questions/Palindrome.java
@@ -0,0 +1,19 @@
+// Question: Check whether a string is palindrome
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+
+        String str = "madam";
+        String rev = "";
+
+        for(int i = str.length()-1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
+
+        if(str.equals(rev))
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
+}
