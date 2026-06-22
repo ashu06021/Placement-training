5fa2adc19a5241ba8beefc69c6efffda2c88d672
Aashish Shinde <aashishshinde.work@gmail.com>
2026-06-22 23:13:30 +0530

    Add files via upload

---
+++ Day16_Questions/modules_demo.py
@@ -0,0 +1,18 @@
+# Question:
+# Demonstrate the use of built-in modules in Python.
+#
+# Concept:
+# Modules are pre-written Python files.
+# We use import keyword to access them.
+
+import math
+
+print("Square Root of 25 =", math.sqrt(25))
+
+print("2 Power 5 =", math.pow(2, 5))
+
+print("Value of Pi =", math.pi)
+
+print("Ceil Value =", math.ceil(4.2))
+
+print("Floor Value =", math.floor(4.8))
